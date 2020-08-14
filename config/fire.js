b2717f4ecb01e0e429f84e19fcd51effd94bd035
afiqnukman <39515215+luckyjet@users.noreply.github.com>
2020-08-15 05:55:04 +0800

    Add files via upload

---
+++ config/fire.js
@@ -0,0 +1,46 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+//Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBzN8vidFZlS4DwreJYxwmjYBXo84bUf74",
+    authDomain: "react-firebase-3d0e4.firebaseapp.com",
+    databaseURL: "https://react-firebase-3d0e4.firebaseio.com",
+    projectId: "react-firebase-3d0e4",
+    storageBucket: "react-firebase-3d0e4.appspot.com",
+    messagingSenderId: "324192513334",
+    appId: "1:324192513334:web:eb22be9197e07b0dc95797",
+    measurementId: "G-K63L6QJXRV"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+// export const db = app.database();
+export default app;
+
+/*if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+} */
+
+/*async componentWillMount();{
+  await firebase.initializeApp({
+  apiKey: "AIzaSyDYxnC4W9DWrlZtSLJKdDhHGswNZiQqd5g",
+  authDomain: "testfirebase-2c0e1.firebaseapp.com",
+  databaseURL: "https://testfirebase-2c0e1.firebaseio.com",
+  projectId: "testfirebase-2c0e1",
+  storageBucket: "testfirebase-2c0e1.appspot.com",
+  messagingSenderId: "35296136453",
+  appId: "1:35296136453:web:2d65a54b87a3e22a25bb6c"
+}); }
+render(); {
+  return (
+    <View>
+      <Header headerText="Authentication" />
+      <LoginForm />
+    </View>
+  );
+}
+} 
+
+export default App;
+*/
